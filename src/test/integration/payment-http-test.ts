b6936789739fcbd7f6d15d03dad082c9d5b53ad3
eddie-yim <eddie2yim@gmail.com>
2019-02-22 17:55:03 +0900

    update test

--- src/test/integration/payment-http-test.ts
+++ src/test/integration/payment-http-test.ts
@@ -7,7 +7,6 @@ import * as jwt from "jsonwebtoken";
 chai.use(require("chai-http"));
 
 const cert = fs.readFileSync(path.resolve(__dirname, "../../resources/ridi-pay_to_ridi-kcp.key"));
-const pem = fs.readFileSync(path.resolve(__dirname, "../../resources/ridi-pay_to_ridi-kcp.key.pub"));
 
 const nextYear = new Date();
 nextYear.setFullYear(new Date().getFullYear() + 1);
@@ -23,47 +22,20 @@ const token = jwt.sign(
         algorithm: "RS256"
     }
 );
-// jwt.sign({
-//     iss: "ridi-pay",
-//     aud: "ridi-kcp",
-//     exp: Math.ceil(nextYear.getTime() / 1000)
-// }, cert, { algorithm: "RS256" }, (err, token) => {
-//     console.log('JWT TOKEN:', token);
-//     console.log("=========");
-//     return token;
-//     // jwt.verify(token, pem, (err, decoded) => {
-//     //     console.log('ERR', err);
-//     //     console.log('DECODED', decoded);
-//     // });
-
-// });
 
 describe("kcp payments api", () => {
-    // it("should return 200 if it exists", (done) => {
-    //     chai
-    //         .request(app)
-    //         .get("/kcp/health")
-    //         .end((err, res) => {
-    //             chai.expect(res).to.have.status(200);
-
-    //             return done();
-    //         });
-    // });
-
     it("배치키 요청 결과 201 상태 반환", (done) => {
         chai.request(app)
             .post("/kcp/payments/auth-key")
             .set("Authorization", `Bearer ${token}`)
             .send({
-                card_no: "1234567800000000",
-                card_expiry_date: "3003",
-                card_tax_no: "790101",
-                card_password: "00",
+                card_no: "5164531234567890",
+                card_expiry_date: "2511",
+                card_tax_no: "940101",
+                card_password: "12",
                 mode: "dev"
             })
-            .end((err, res) => {
-                //console.info('err', err);
-                //console.info('res', res);
+            .end((_, res) => {
                 chai.expect(res).to.have.status(201);
                 return done();
             });
