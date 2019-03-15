c0f378e50ed057f87c79371878c374d6e3810a1f
eddie <eddie@eddie-store.local>
2019-03-15 19:39:52 +0900

    trim run.ts

--- src/test/run.ts
+++ src/test/run.ts
@@ -1,22 +1,19 @@
 import * as fs from "fs";
 import * as mocha from "mocha";
 import * as path from "path";
-import { Database } from "../database";
 
+const suite = new mocha();
 
-	const suite = new mocha();
+fs.readdir(path.join(__dirname, "integration"), (err, files) => {
+	if (err) {
+		throw err;
+	}
 
-	fs.readdir(path.join(__dirname, "integration"), (err, files) => {
-		if (err) {
-			throw err;
-		}
-	
-		files.filter((filename: string) => (filename.match(/\.ts$/))).map((filename) => {
-			suite.addFile(path.join(__dirname, "integration", filename));
-		});
-	
-		suite.run((failures: number) => {
-			process.exit(failures);
-		});
+	files.filter((filename: string) => (filename.match(/\.ts$/))).map((filename) => {
+		suite.addFile(path.join(__dirname, "integration", filename));
 	});
 
+	suite.run((failures: number) => {
+		process.exit(failures);
+	});
+});
