c507df65af522a76fd594a8cc60c54009328d30d
eddie.yim <eddie.yim@ridi.com>
2019-03-22 21:10:40 +0900

    add ReponseLoggingMiddleware

---
+++ src/presentation/middlewares/ResponseLoggingMiddleware.ts
@@ -0,0 +1,18 @@
+import * as Logger from 'bunyan';
+import { Request } from 'express';
+import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
+import { Inject } from 'typedi';
+
+@Middleware({ type: 'after' })
+export class ResponseLoggingMiddleware implements ExpressMiddlewareInterface {    
+    @Inject('logger')
+    logger: Logger;
+
+    use(request: any, response: any, next: (err?: any) => any) {
+        const req = request as Request;
+
+        this.logger.info(`${req.method} ${req.url}`, response);
+
+        next();
+    }
+}
