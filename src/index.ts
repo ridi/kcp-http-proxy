bdc6cfbb211f36f32524ee255125d1e5d0488bca
Gyu Kang <me@amolorsy.me>
2019-06-14 17:23:20 +0900

    Use only 80 port

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import 'reflect-metadata';
 (async () => {
     const app: Application = App.init();
     
-    const port = process.env.PORT;        
+    const port = 80;
     const server = app.listen(port, () => {
         console.info(`listening on port ${port}`);
     });
